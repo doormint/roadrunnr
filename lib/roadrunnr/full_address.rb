2b2286da3c58f24ff6859c7234d77521b127f0eb
Manthan <mntdamania@gmail.com>
2015-10-22 17:57:19 +0530

    fix namespace

--- lib/roadrunnr/full_address.rb
+++ lib/roadrunnr/full_address.rb
@@ -1,87 +1,90 @@
-class FullAddress
-  attr_accessor :address, :locality, :sub_locality, :city, :geo
+module Roadrunnr
+  class FullAddress
+    attr_accessor :address, :locality, :sub_locality, :city, :geo
 
-  def initialize
-    @locality = create_locality
-    @sub_locality = create_sub_locality
-    @city = create_city
-    @geo = create_geo
-  end
-
-  def create_locality
-    Locality.new
-  end
-
-  def create_sub_locality
-    SubLocality.new
-  end
-
-  def create_city
-    City.new
-  end
-
-  def create_geo
-    Geo.new
-  end
-
-  def valid?
-    address.present? && locality.valid? && sub_locality.valid? && city.valid? && geo.valid?
-  end
-
-  def add_full_address(address, locality_name, sub_locality_name, city_name, geo_latitude, geo_longitude)
-    @address = address
-    @locality.add_name(locality_name)
-    @sub_locality.add_name(sub_locality_name)
-    @city.add_name(city_name)
-    @geo.add_geo(geo_latitude, geo_longitude)
-  end
-
-  def save
-      {
-        'address'=> @address,
-        'locality'=> @locality.save,
-        'sub_locality'=> @sub_locality.save,
-        'city'=> @city.save,
-        'geo'=> @geo.save
-      }
+    def initialize
+      @locality = create_locality
+      @sub_locality = create_sub_locality
+      @city = create_city
+      @geo = create_geo
     end
-end
 
-class Name
-  attr_accessor :name
+    def create_locality
+      Locality.new
+    end
 
-  def add_name(name)
-    @name = name
+    def create_sub_locality
+      SubLocality.new
+    end
+
+    def create_city
+      City.new
+    end
+
+    def create_geo
+      Geo.new
+    end
+
+    def valid?
+      address.present? && locality.valid? && sub_locality.valid? && city.valid? && geo.valid?
+    end
+
+    def add_full_address(address, locality_name, sub_locality_name, city_name, geo_latitude, geo_longitude)
+      @address = address
+      @locality.add_name(locality_name)
+      @sub_locality.add_name(sub_locality_name)
+      @city.add_name(city_name)
+      @geo.add_geo(geo_latitude, geo_longitude)
+    end
+
+    def save
+        {
+          'address'=> @address,
+          'locality'=> @locality.save,
+          'sub_locality'=> @sub_locality.save,
+          'city'=> @city.save,
+          'geo'=> @geo.save
+        }
+      end
   end
 
-  def valid?
-    name.present?
+  class Name
+    attr_accessor :name
+
+    def add_name(name)
+      @name = name
+    end
+
+    def valid?
+      name.present?
+    end
+
+    def save
+      { 'name'=> @name }
+    end
   end
 
-  def save
-    { 'name'=> @name }
+  class Locality < Name;end
+
+  class SubLocality < Name;end
+
+  class City < Name;end
+
+  class Geo
+    attr_accessor :latitude, :longitude
+
+    def add_geo(latitude, longitude)
+      @latitude = latitude
+      @longitude = longitude
+    end
+
+    def valid?
+      latitude.present? && longitude.present?
+    end
+
+    def save
+      { 'latitude'=> @latitude, 'longitude'=> @longitude }
+    end
   end
-end
 
-class Locality < Name;end
-
-class SubLocality < Name;end
-
-class City < Name;end
-
-class Geo
-  attr_accessor :latitude, :longitude
-
-  def add_geo(latitude, longitude)
-    @latitude = latitude
-    @longitude = longitude
-  end
-
-  def valid?
-    latitude.present? && longitude.present?
-  end
-
-  def save
-    { 'latitude'=> @latitude, 'longitude'=> @longitude }
-  end
 end
