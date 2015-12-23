e894e719d652fc9fdae884409e8c436b0a0f0ff9
Sowmya <sowmya.iyer@doormint.in>
2015-12-23 16:56:59 +0530

    call the ship api

--- lib/roadrunnr/request.rb
+++ lib/roadrunnr/request.rb
@@ -13,7 +13,7 @@ module Roadrunnr
     end
 
     def create_order(order_object)
-      HTTParty.post(url + "v1/orders", :body => order_object.save.to_json, :headers => {'Content-Type' => 'application/json', "Authorization" => "Token #{self.access_token}"})
+      HTTParty.post(url + "v1/orders/ship", :body => order_object.save.to_json, :headers => {'Content-Type' => 'application/json', "Authorization" => "Token #{self.access_token}"})
     end
 
     def cancel_order(order_id)
